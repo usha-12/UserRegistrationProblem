a39ff31af3312c418f476b68dbc1324cfff0b3e5
usha-12 <usha12ahirwar@gmail.com>
2022-11-08 12:08:33 +0530

    Check first name valide or not

--- src/com/brideglabz/registrationproblem/RegistrationProblem.java
+++ src/com/brideglabz/registrationproblem/RegistrationProblem.java
@@ -1,2 +1,9 @@
-package com.brideglabz.registrationproblem;public class RegistrationProblem {
-}
+package com.brideglabz.registrationproblem;
+public class RegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Regular Expressions program to validate PIN code!");
+            Validation validation=new Validation();
+            validation.checkFirstName();
+
+        }
+    }
